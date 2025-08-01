9155de6a7fc3e2f5d3f0c39d56cb1ae710d7ea71
Marlon Costa <marlonsc@gmail.com>
2025-08-01 17:30:09 -0300

    0.9.0

--- src/flexcore/__init__.py
+++ src/flexcore/__init__.py
@@ -1,10 +1,10 @@
 """FLEXT FlexCore - Event-driven architecture system."""
 
+from flexcore.core import FlexCore
+
 __version__ = "0.9.0"
 __author__ = "FLEXT Team"
 __email__ = "team@flext.dev"
 
-# Core exports for the flexcore package
-from flexcore.core import FlexCore
 
 __all__ = ["FlexCore", "__version__"]
