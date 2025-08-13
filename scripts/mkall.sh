49d39276abfa066281a42bafa9324387c63335be
Marlon Costa <marlonsc@gmail.com>
2025-08-13 18:10:03 -0300

    0.9.0

--- scripts/mkall.sh
+++ scripts/mkall.sh
@@ -26,7 +26,7 @@ case "$1" in
 	for i in zsyscall*go; do
 		# Run the command line that appears in the first line
 		# of the generated file to regenerate it.
-		sed 1q "$i" | sed 's;^// ;;' | sh >_"$i" && gofmt <_$i >$i
+		sed 1q "$i" | sed 's;^// ;;' | sh >_"$i" && gofmt <_"$i" >$i
 		rm _$i
 	done
 	exit 0
@@ -50,7 +50,7 @@ if [[ $GOOS == "linux" ]]; then
 	# Use the Docker-based build system
 	# Files generated through docker (use $cmd so you can Ctl-C the build or run)
 	$cmd docker build --tag generate:"$GOOS" "$GOOS"
-	$cmd docker run --interactive --tty --volume $(cd -- "$(dirname -- "$0")/.." && pwd):/build generate:$GOOS
+	$cmd docker run --interactive --tty --volume $(cd -- "$(dirname -- "$0")/.." && pwd):/build generate:"$GOOS"
 	exit
 fi
 
