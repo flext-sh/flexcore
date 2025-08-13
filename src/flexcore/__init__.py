1e620e7ca7a0b658ee8fdc0a21ff3f6e3750313b
Marlon Costa <marlonsc@gmail.com>
2025-08-13 15:08:32 -0300

    0.9.0

--- src/flexcore/__init__.py
+++ src/flexcore/__init__.py
@@ -11,8 +11,9 @@ from flexcore.core import FlexCore
 from importlib.metadata import version
 
 __version__ = version("flexcore")
+__version_info__ = tuple(int(x) for x in __version__.split(".") if x.isdigit())
 __author__ = "FLEXT Team"
 __email__ = "team@flext.dev"
 
 
-__all__: list[str] = ["FlexCore", "__version__"]
+__all__: list[str] = ["FlexCore", "__version__", "__version_info__"]
