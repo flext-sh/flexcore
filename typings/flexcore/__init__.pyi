d067d76be995382e2856911fedca86e59a7125b9
Marlon Costa <marlonsc@gmail.com>
2025-08-19 22:01:27 -0300

    0.9.0

--- typings/flexcore/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from _typeshed import Incomplete
-
-from flexcore.core import FlexCore as FlexCore
-
-__all__ = ["FlexCore", "__version__", "__version_info__"]
-
-__version__: Incomplete
-__version_info__: Incomplete
